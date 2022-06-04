731e3b52d29a4f9a00a59176e57778d9fd909c07
Stefan Brandl <steevie.brandl@gmail.com>
2022-06-04 17:29:18 +0200

    introducing pep8 styling

--- book.py
+++ book.py
@@ -1,71 +1,71 @@
+import pygame
 import random
 import sys
 sys.path.append("/opt/homebrew/lib/python3.9/site-packages/")
-import pygame
 
-rgbBrown  = (139,69, 19)
-rgbSilk   = (255,248,220)
-rgbGolden = (218,165,32)
-rgbRed    = (255,0,  0)
-rgbGreen  = (0,  255,0)
-rgbBlue   = (0,  0,  255)
-rgbYellow = (255,255,0)
-rgbGray   = (128,128,128)
-rgbBlack  = (0,  0,  0)
-rgbWhite  = (255,255,255)
+rgbBrown = (139, 69, 19)
+rgbSilk = (255, 248, 220)
+rgbGolden = (218, 165, 32)
+rgbRed = (255, 0,  0)
+rgbGreen = (0,  255, 0)
+rgbBlue = (0,  0,  255)
+rgbYellow = (255, 255, 0)
+rgbGray = (128, 128, 128)
+rgbBlack = (0,  0,  0)
+rgbWhite = (255, 255, 255)
+
 
 class Book:
-	shelfThickness = 5
-	xOffset = 20
-	yOffset = 24
-	width   = 5
-	height  = 54
-	row     = 0
-	col     = 0
-	x 	    = 0
-	y       = 0
-	lBound  = 1
+    shelfThickness = 5
+    xOffset = 20
+    yOffset = 24
+    width = 5
+    height = 54
+    row = 0
+    col = 0
+    x = 0
+    y = 0
+    lBound = 1
 
-	def __init__(self, title="There and back again", author="Bilbo Baggins"):
-		self.title  = title
-		self.author = author
-		self.clr    = rgbSilk
+    def __init__(self, title="There and back again", author="Bilbo Baggins"):
+        self.title = title
+        self.author = author
+        self.clr = rgbSilk
 
-	def getDetails(self):
-		print( self.title + " by " + self.author + " -> " + self.genre )
-		print( "\tis stored in Row " + str(self.row) + " and Spot " + str(self.col) )
+    def getDetails(self):
+        print(self.title + " by " + self.author + " -> " + self.genre)
+        print("\tis stored in Row " + str(self.row) +
+              " and Spot " + str(self.col))
 
-	def getDimensions(self):
-		return (self.x,self.y,self.width,self.height)
+    def getDimensions(self):
+        return (self.x, self.y, self.width, self.height)
 
-	def drawSelf(self, screen):
-		pygame.draw.rect(screen, self.clr, self.getDimensions())
+    def drawSelf(self, screen):
+        pygame.draw.rect(screen, self.clr, self.getDimensions())
 
-	def setSpot(self,r,c):
-		self.setRow(r)
-		self.setCol(c)
+    def setSpot(self, r, c):
+        self.setRow(r)
+        self.setCol(c)
 
-	def setRow(self,r):
-		self.row = r
-		self.y   = r * (self.height + self.shelfThickness) + self.yOffset
+    def setRow(self, r):
+        self.row = r
+        self.y = r * (self.height + self.shelfThickness) + self.yOffset
 
-	def setCol(self,c):
-		self.col = c
-		self.x   = c * (self.width + self.lBound)          + self.xOffset
+    def setCol(self, c):
+        self.col = c
+        self.x = c * (self.width + self.lBound) + self.xOffset
 
-	def setTitle(self,name):
-		self.title = name
+    def setTitle(self, name):
+        self.title = name
 
-	def setAuthor(self,name):
-		self.author = name
+    def setAuthor(self, name):
+        self.author = name
 
-	def setGenre(self, genre):
-		self.genre = genre
+    def setGenre(self, genre):
+        self.genre = genre
 
-	def setColor(self, rgb):
-		self.clr = rgb
+    def setColor(self, rgb):
+        self.clr = rgb
 
-	def getRandomRGB(self):
-		return (random.randrange(0, 256),random.randrange(0, 256),random.randrange(0, 256))
-
-
+    def getRandomRGB(self):
+        return (random.randrange(0, 256), random.randrange(0, 256), random.randrange(0, 256))
--- pyBook.py
+++ pyBook.py
@@ -1,56 +1,59 @@
+import pygame
 import sys
 
 from book import *
 from bookShelf import Bookshelf
 
 sys.path.append("/opt/homebrew/lib/python3.9/site-packages/")
-import pygame
 
 pygame.init()
-screen = pygame.display.set_mode([800,600])
+screen = pygame.display.set_mode([800, 600])
 clock = pygame.time.Clock()
 
 b1 = Book("The Hound of the Baskervilles", "Arthur Conan Doyle")
-b1.setSpot(0,0)
+b1.setSpot(0, 0)
 b2 = Book("The Hobbit", "J. R. R. Tolkien")
-b2.setSpot(1,1)
+b2.setSpot(1, 1)
 b3 = Book()
 bs = Bookshelf()
 
 bookList = []
 for i in range(10):
-	bookList.append(Book())
+    bookList.append(Book())
 
-x      = 0
-y      = 0
-speed  = 1
-fps    = 60
+x = 0
+y = 0
+speed = 1
+fps = 60
+
 
 def detectKeys():
-	global x
-	global y
-	global speed
-	pressed = pygame.key.get_pressed()
-	if pressed[pygame.K_UP]:
-		y -= speed	
-	if pressed[pygame.K_RIGHT]:
-		x += speed
-	if pressed[pygame.K_DOWN]:
-		y += speed
-	if pressed[pygame.K_LEFT]:
-		x -= speed
+    global x
+    global y
+    global speed
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_UP]:
+        y -= speed
+    if pressed[pygame.K_RIGHT]:
+        x += speed
+    if pressed[pygame.K_DOWN]:
+        y += speed
+    if pressed[pygame.K_LEFT]:
+        x -= speed
+
 
 go = True
 while go:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT: sys.exit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
 
-	detectKeys()
+    detectKeys()
 
-	screen.fill(rgbBlack)
+    screen.fill(rgbBlack)
 
-	bs.drawSelf(screen)
-	bs.drawBooks(screen)
+    bs.drawSelf(screen)
+    bs.drawBooks(screen)
 
-	pygame.display.update()
-	clock.tick(fps)
+    pygame.display.update()
+    clock.tick(fps)
